a9b1b797f0c87bd08cbdf53adcbbfd798651097e
alobikov <aleksej.lobikov@gmail.com>
2020-11-22 10:35:46 +0200

    add MessagesScreen view

--- App.js
+++ App.js
@@ -5,14 +5,16 @@ import Welcome from "./app/screens/WelcomeScreen";
 import Card from "./app/components/Card";
 import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import MessagesScreen from "./app/screens/MessagesScreen";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       {/* <ListingDetailsScreen /> */}
       {/* <Welcome /> */}
-      <ViewImageScreen />
+      {/* <ViewImageScreen /> */}
       {/* <StatusBar style="auto" /> */}
+      <MessagesScreen />
     </SafeAreaView>
   );
 }
---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, FlatList } from "react-native";
+import ListItem from "../components/ListItem";
+
+const messages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/aleksej_sm.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/mela_sad_sm.jpg"),
+  },
+];
+
+export default function MessagesScreen() {
+  return (
+    <FlatList
+      data={messages}
+      keyExtractor={(messages) => messages.id.toString()}
+      renderItem={({ item }) => (
+        <ListItem
+          title={item.title}
+          subTitle={item.description}
+          image={item.image}
+        />
+      )}
+    ></FlatList>
+  );
+}
+
+const styles = StyleSheet.create({});
